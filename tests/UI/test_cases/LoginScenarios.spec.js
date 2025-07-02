dfb63f44e14a8d481b1a6ff2964527afae94e4ce
Jesmin <jesminjoykutty12@gmail.com>
2025-07-02 17:47:52 +1000

    just formatting fixes

--- tests/UI/test_cases/LoginScenarios.spec.js
+++ tests/UI/test_cases/LoginScenarios.spec.js
@@ -10,30 +10,31 @@ Steps:
 
 Expected Result: User lands on the inventory page 
 */
-import test from  '../../../fixtures/fixtures'
-const Inventory = require('../pages/inventoryPage')
-const testData = require('../../../data/testdata.json');
+import test from "../../../fixtures/fixtures";
+const Inventory = require("../pages/inventoryPage");
+const testData = require("../../../data/testdata.json");
 const { validUser, lockedOutUser, invalidUser, emptyCredentials } = testData;
 
-test.describe('Positive Login Scenarios', () => {
+test.describe("Positive Login Scenarios", () => {
+  test("TC_01 : verify successful login to inventory page : @smoke", async ({
+    page,
+    loginPage,
+  }) => {
+    await test.step("verify Login Page components ", async () => {
+      await loginPage.loadURL();
+      await loginPage.verifyLoginPageComponents();
+    });
 
-    test('TC_01 : verify successful login to inventory page : @smoke',async ({page,loginPage})=>{
+    await test.step("verify successful Login ", async () => {
+      await loginPage.login(validUser.username, validUser.password);
 
-        await test.step('verify Login Page components ',async ()=>{
-            await loginPage.loadURL()
-            await loginPage.verifyLoginPageComponents()
-        })
-
-        await test.step('verify successful Login ',async ()=>{
-            await loginPage.login(validUser.username,validUser.password)
-
-            const inventory = new Inventory(page);
-            await inventory.verifyInventoryPageUrl()
-        })
-    })
-})
-test.describe('Negative Login Scenarios', () => {
-    /*TC02: Login with locked-out user
+      const inventory = new Inventory(page);
+      await inventory.verifyInventoryPageUrl();
+    });
+  });
+});
+test.describe("Negative Login Scenarios", () => {
+  /*TC02: Login with locked-out user
 
     Steps:
     1.Enter username: locked_out_user
@@ -42,15 +43,15 @@ test.describe('Negative Login Scenarios', () => {
 
     Expected: Error message shown: "Sorry, this user has been locked out."
     */
-    test.beforeEach(async ({ loginPage }) => {
-      await loginPage.loadURL();
-    });
+  test.beforeEach(async ({ loginPage }) => {
+    await loginPage.loadURL();
+  });
 
-    test('TC_02 : verify login as a locked-out user',async ({loginPage})=>{
-        await loginPage.login(lockedOutUser.username,lockedOutUser.password)
-        await loginPage.verifyLockOutError()
-    })
-    /*TC003: Login with invalid credentials
+  test("TC_02 : verify login as a locked-out user", async ({ loginPage }) => {
+    await loginPage.login(lockedOutUser.username, lockedOutUser.password);
+    await loginPage.verifyLockOutError();
+  });
+  /*TC003: Login with invalid credentials
 
     Steps:
     1.Navigate to login page
@@ -60,11 +61,13 @@ test.describe('Negative Login Scenarios', () => {
 
     Expected: Error message shown: "Username and password do not match any user"
     */
-    test('TC_03 : verify login with invalid credentials',async ({loginPage})=>{
-        await loginPage.login(invalidUser.username,invalidUser.password)
-        await loginPage.verifyInvalidUserError()
-    })
-    /*TC004: Login with empty credentials
+  test("TC_03 : verify login with invalid credentials", async ({
+    loginPage,
+  }) => {
+    await loginPage.login(invalidUser.username, invalidUser.password);
+    await loginPage.verifyInvalidUserError();
+  });
+  /*TC004: Login with empty credentials
 
     Steps:
     1.Navigate to login page
@@ -73,8 +76,10 @@ test.describe('Negative Login Scenarios', () => {
 
     Expected: Error message shown: "Username is required"
     */
-    test('TC_04 : verify login with empty credeentials',async ({loginPage})=>{
-        await loginPage.login(emptyCredentials.username,emptyCredentials.password)
-        await loginPage.verifyEmptyCredentialsError()
-    })
-})
+  test("TC_04 : verify login with empty credeentials", async ({
+    loginPage,
+  }) => {
+    await loginPage.login(emptyCredentials.username, emptyCredentials.password);
+    await loginPage.verifyEmptyCredentialsError();
+  });
+});
